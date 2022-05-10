f774a3c7d57e6b5709360b7cdaa6e28bca4f8537
huytran119411 <huytran119411@gmail.com>
2022-05-10 14:53:13 +0700

    add file

--- kiem-tra-nam-nhuan.js
+++ kiem-tra-nam-nhuan.js
@@ -1,7 +1,7 @@
 let year = parseInt(prompt("Enter a year"));
-if(year % 4 == 0) {
-    if (year % 100 == 0) {
-        if (year % 400 == 0) {
+if(year % 4 === 0) {
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
             document.write(year + " is a leap year");
         } else {
             document.write(year + " is NOT a leap year");
